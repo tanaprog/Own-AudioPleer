bf68c17dcc7df426899db8b94f2026fb1f063c8e
tanaprog <tananaeva.art@gmail.com>
2023-11-21 21:27:02 +0300

    united some functions in one function

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const music = [
     {
         title: 'Space Melody',
         autor: 'Vize and Alan Wolker',
-        path: 'media/living-without-you.mp3'
+        path: 'media/space-melody.mp3'
     },
     {
         title: 'Road to hell',
@@ -20,91 +20,99 @@ const music = [
     {
         title: 'Till I Come',
         autor: 'ATB',
-        path: 'media/space-melody.mp3'
+        path: 'media/till-i-come.mp3'
     },
     {
         title: 'Living Without You',
         autor: 'Rasmus',
-        path: 'media/till-i-come.mp3'
+        path: 'media/living-without-you.mp3'
     },
 ];
 
 let musicIndex = 0;
 
-function playPause() {
-    if (ctrlIcon.classList.contains('fa-pause')) {
-        song.pause();
-        ctrlIcon.classList.remove('fa-pause');
-        ctrlIcon.classList.add('fa-play');
+function init() {
+
+    function playPause() {
+        if (ctrlIcon.classList.contains('fa-pause')) {
+            song.pause();
+            ctrlIcon.classList.remove('fa-pause');
+            ctrlIcon.classList.add('fa-play');
+        }
+        else {
+            song.play();
+            ctrlIcon.classList.add('fa-pause');
+            ctrlIcon.classList.remove('fa-play');
+        }
     }
-    else {
+
+    function loadSong(musicIndex) {
+        title.innerHTML = music[musicIndex].title;
+        description.innerHTML = music[musicIndex].autor;
+        song.src = music[musicIndex].path;
+    }
+
+    function clickForward() {
+        musicIndex++;
+
+        if (musicIndex > music.length - 1) {
+            musicIndex = 0;
+            loadSong(musicIndex);
+        }
+        else {
+            loadSong(musicIndex);
+            song.play();
+        }
+
         song.play();
         ctrlIcon.classList.add('fa-pause');
         ctrlIcon.classList.remove('fa-play');
     }
-}
 
-function loadSong(musicIndex) {
-    title.innerHTML = music[musicIndex].title;
-    description.innerHTML = music[musicIndex].autor;
-    song.src = music[musicIndex].path;
-}
+    function clickBackward() {
+        musicIndex--;
 
-function clickForward() {
-    musicIndex++;
+        if (musicIndex < 0) {
+            musicIndex = music.length - 1;
+            loadSong(musicIndex);
+        }
+        else {
+            loadSong(musicIndex);
+            song.play();
+        }
 
-    if (musicIndex > music.length - 1) {
-        musicIndex = 0;
-        loadSong(musicIndex);
-    }
-    else {
-        loadSong(musicIndex);
         song.play();
+        ctrlIcon.classList.add('fa-pause');
+        ctrlIcon.classList.remove('fa-play');
     }
 
-    song.play();
-    ctrlIcon.classList.add('fa-pause');
-    ctrlIcon.classList.remove('fa-play');
-}
+    ctrlIcon.addEventListener('click', playPause);
+    forward.addEventListener('click', clickForward);
+    backward.addEventListener('click', clickBackward);
+    song.addEventListener('ended', clickForward);
 
-function clickBackward() {
-    musicIndex--;
-
-    if (musicIndex < 0) {
-        musicIndex = music.length - 1;
-        loadSong(musicIndex);
-    }
-    else {
-        loadSong(musicIndex);
-        song.play();
-    }
-
-    song.play();
-    ctrlIcon.classList.add('fa-pause');
-    ctrlIcon.classList.remove('fa-play');
-}
-
-ctrlIcon.addEventListener('click', playPause);
-forward.addEventListener('click', clickForward);
-backward.addEventListener('click', clickBackward);
-song.addEventListener('ended', clickForward);
-
-song.onloadedmetadata = function () {
-    progress.max = song.duration;
-    progress.value = song.currentTime;
-}
-
-progress.onchange = function () {
-    song.play();
-    song.currentTime = progress.value;
-}
-
-if (song) {
-    setInterval(() => {
+    song.onloadedmetadata = function () {
+        progress.max = song.duration;
         progress.value = song.currentTime;
-    }, 500);
-}
+    }
 
-loadSong(musicIndex);
+    progress.onchange = function () {
+        song.play();
+        song.currentTime = progress.value;
+    }
+
+    if (song) {
+        setInterval(() => {
+            progress.value = song.currentTime;
+        }, 500);
+    }
+
+    loadSong(musicIndex);
+
+};
+
+init();
+
+
 
 
